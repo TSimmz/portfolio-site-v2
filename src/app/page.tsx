a5067b38e904262f672a9b36110733cca44fcbed
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-08-12 02:16:26 -0400

    Updates `Page` to be a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-'use client';
-
 import About from '~/sections/About';
 import Contact from '~/sections/Contact';
 import Hero from '~/sections/Hero';
 import Portfolio from '~/sections/Portfolio';
-import { api } from '~/utils/api';
 
 function HomePage() {
   return (
@@ -17,4 +14,4 @@ function HomePage() {
   );
 }
 
-export default api.withTRPC(HomePage);
+export default HomePage;
