ea7191bb239943f1be38cdd835b5215ccd0accf1
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-08-21 14:48:42 -0400

    Adds 3D rotation to portfolio cards

--- src/sections/Portfolio/PortfolioCard.tsx
+++ src/sections/Portfolio/PortfolioCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import { type FC } from 'react';
+import { type FC, useRef, useEffect } from 'react';
+import { useMouseHovered } from 'react-use';
 
 type PortfolioCardProps = {
   title: string;
@@ -14,9 +15,57 @@ const PortfolioCard: FC<PortfolioCardProps> = ({
   href,
   imageUrl = '',
 }) => {
+  const cardRef = useRef<HTMLAnchorElement>(null);
+
+  const mousePosition = useMouseHovered(cardRef, {
+    bound: true,
+    whenHovered: true,
+  });
+
+  const cardSize = useRef<{ w: number; h: number }>({
+    w: mousePosition.elW,
+    h: mousePosition.elH,
+  });
+
+  const mapValues = (
+    value: number,
+    inMin: number,
+    inMax: number,
+    outMin: number,
+    outMax: number,
+  ) => {
+    return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+  };
+
+  useEffect(() => {
+    if (cardRef.current !== null) {
+      if (cardSize.current.w === 0 || cardSize.current.h === 0)
+        cardSize.current = { w: mousePosition.elW, h: mousePosition.elH };
+
+      const height = cardSize.current.h;
+      const width = cardSize.current.w;
+      const center = { x: Math.floor(width / 2), y: Math.floor(height / 2) };
+
+      const posX = mousePosition.elX;
+      const posY = mousePosition.elY;
+
+      const ratioX = Math.abs(center.x - posX);
+      const ratioY = Math.abs(center.y - posY);
+
+      const modifierX = posX > center.x ? 1 : -1;
+      const rotateY = mapValues(ratioX, 0, center.x, 0, 15) * modifierX;
+      const modifierY = posY > center.y ? -1 : 1;
+      const rotateX = mapValues(ratioY, 0, center.y, 0, 15) * modifierY;
+
+      cardRef.current.style.setProperty('--_rotateX', `${rotateX}deg`);
+      cardRef.current.style.setProperty('--_rotateY', `${rotateY}deg`);
+    }
+  }, [mousePosition]);
+
   return (
     <Link
-      className="portfolio-card group relative flex min-h-[200px] max-w-sm flex-col gap-4 overflow-hidden rounded-lg text-light-base backdrop-brightness-110 transition-colors duration-300 ease-in-out hover:backdrop-brightness-125 dark:text-dark-base"
+      ref={cardRef}
+      className="portfolio-card three-dee group relative flex min-h-[200px] max-w-sm flex-col gap-4 overflow-hidden rounded-lg text-light-base backdrop-brightness-110 transition-colors duration-300 ease-in-out hover:backdrop-brightness-125 dark:text-dark-base"
       href={href}
       target="_blank"
     >
