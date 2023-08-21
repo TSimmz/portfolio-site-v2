8c58df3a77103d3ffe6be17a0bf92390b62a011d
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-08-21 13:21:27 -0400

    Adds Navbar hiding on scroll

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,13 +2,8 @@
 
 import { usePathname } from 'next/navigation';
 import { useRef, useState, useCallback, useEffect, useMemo } from 'react';
-import { useToggle, useWindowSize } from 'react-use';
-import {
-  motion,
-  //useScroll,
-  //useTransform,
-  AnimatePresence,
-} from 'framer-motion';
+import { useToggle, useWindowSize, useWindowScroll } from 'react-use';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import theme from 'tailwindcss/defaultTheme';
 
@@ -17,7 +12,6 @@ import {
   type BaseRouteKeys,
   baseRouteKeysList,
   baseRoutes,
-  //localStorageHasScrolledId,
 } from '~/utils/constants';
 import Burger from './buttons/Burger';
 import BrandLogo from './svgs/BrandLogo';
@@ -27,12 +21,14 @@ import ResumeButton from './buttons/ResumeButton';
 
 const NavMenu = () => {
   const pathname = usePathname();
-  // const { scrollY } = useScroll();
-  // const opacity = useTransform(scrollY, [100, 500], [0, 1]);
+  const { y } = useWindowScroll();
+
+  const prevScrollY = useRef<number>(y);
+
+  const position = useRef<number>(0);
+
   const { width } = useWindowSize();
   const [opened, toggle] = useToggle(false);
-
-  // const [hasScrolled, setHasScrolled] = useState<boolean>(false);
 
   const navMenuRef = useRef<HTMLElement | null>(null);
 
@@ -73,17 +69,21 @@ const NavMenu = () => {
     [navMenuRef],
   );
 
-  // const opacityValue = opacity.isAnimating();
-  // useEffect(() => {
-  //   console.log('Scrolling...', hasScrolled);
-  //   if (
-  //     localStorage[localStorageHasScrolledId] === 'false' &&
-  //     opacity.get() === 1
-  //   ) {
-  //     localStorage[localStorageHasScrolledId] = 'true';
-  //     setHasScrolled(true);
-  //   }
-  // }, [opacityValue]);
+  useEffect(() => {
+    if (y >= 500) {
+      if (prevScrollY.current <= y) {
+        if (Math.abs(prevScrollY.current - y) >= 200) {
+          position.current = -100;
+          prevScrollY.current = y;
+        }
+      } else {
+        if (Math.abs(prevScrollY.current - y) >= 200) {
+          position.current = 0;
+          prevScrollY.current = y;
+        }
+      }
+    }
+  }, [y]);
 
   useEffect(() => {
     toggle(false);
@@ -217,9 +217,9 @@ const NavMenu = () => {
 
   return (
     <motion.header
-      //style={{ opacity: pathname === '/' && !hasScrolled ? opacity : 1 }}
       id="page-header"
       key="page-header"
+      animate={{ translateY: position.current }}
       className={`fixed top-0 z-20 w-full bg-neutrals-600 py-3 pl-4 pr-4 shadow-2xl shadow-neutrals-300/90 backdrop-blur-md dark:bg-neutrals-700 dark:shadow-neutrals-800 sm:pr-2`}
     >
       <nav
